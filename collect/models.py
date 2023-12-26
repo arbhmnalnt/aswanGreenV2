7f81138634651509705df037bb6b15c2233e80df
ahmed sayed <ahmedsayed124680@gmail.com>
2023-12-26 21:11:30 +0200

    start collect app works

--- collect/models.py
+++ collect/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from track.models import TimeStampMixin
 
 # Create your models here.
+class CollectRequest(TimeStampMixin,models.Model):
+    clientt     = models.ManyToManyField()
+    collector   = models.IntegerField(default=0,null=True, blank=True)
+    date
+
+    def __str__(self):
+        return self.name
